77c900fd3df26a88c968b01511768be83eb2c2eb
Mia zhou <1160019532@qq.com>
2021-04-14 09:52:17 +0800

    websocket project

--- src/main/java/com/cn/demo/config/WebSocketConfig.java
+++ src/main/java/com/cn/demo/config/WebSocketConfig.java
@@ -17,25 +17,32 @@ public class WebSocketConfig {
 
     @Bean
     public WebSocketConnectionManager webSocketConnectionManager() {
-        String bearerToken = "eyJhbGciOiJFUzI1NiIsIng1dCI6IjhGQzE5Qjc0MzFCNjNFNTVCNjc0M0QwQTc5MjMzNjZCREZGOEI4NTAifQ.eyJvYWEiOiI3Nzc3NSIsImlzcyI6Im9hIiwiYWlkIjoiMTA5IiwidWlkIjoielE3UE1EZUJzRk5COXlrMXBwYmJGdz09IiwiY2lkIjoielE3UE1EZUJzRk5COXlrMXBwYmJGdz09IiwiaXNhIjoiRmFsc2UiLCJ0aWQiOiIyMDAyIiwic2lkIjoiYTIyMjk5ZWIzZTdkNDI2N2JhYzY1YTkyMzZmOWFjMWEiLCJkZ2kiOiI4NCIsImV4cCI6IjE2MTgzMDA5MTIifQ._jGhhpeW1G3Yp8hJUQwzGzj-BE9uZtEwq0mI2TXMPvDblx6pjTXdN71Zc2xedMtXDN9mn23_HW-V3MqEZYSA2w";
-        String contextId = "explorer_1618218099024";
-        String connectToken = "FD5z3cTYboXTp%2Fy7gVAtA1A8gY%2F416XEOp6Nbapvc1IcZx5VEcJSVtSibzYPCS7nxwCnM64vIuWGmMnOWRNIYR8LMoFPbeLSpGXIXTSgWi%2FCQ9Dc";
-        String tid = "3";
+        String bearerToken = "eyJhbGciOiJFUzI1NiIsIng1dCI6IjhGQzE5Qjc0MzFCNjNFNTVCNjc0M0QwQTc5MjMzNjZCREZGOEI4NTAifQ.eyJvYWEiOiI3Nzc3NSIsImlzcyI6Im9hIiwiYWlkIjoiMTA5IiwidWlkIjoielE3UE1EZUJzRk5COXlrMXBwYmJGdz09IiwiY2lkIjoielE3UE1EZUJzRk5COXlrMXBwYmJGdz09IiwiaXNhIjoiRmFsc2UiLCJ0aWQiOiIyMDAyIiwic2lkIjoiZThjYzZlYjNiNmViNDE2ODkzNTczMThjYzU1M2FiOWIiLCJkZ2kiOiI4NCIsImV4cCI6IjE2MTg0NTA1MTgifQ.OiN5BgUA0glhCIUcYhBK7JALOD7LI3S81epdWRd_d6rn0SDcJtVZCpW1RVl81tnDTxr2ivlrz99KBeF2GHnYqA";
+        String contextId = "explorer_1618364123021";
+        String connectToken = "aKOob%2FmTsJTA6L7RlFbEbeVSxxQCHvLB6T%2FawtnWGzSVTRtED6sIq42B0IiEcsfXplKkapv742lzjxKbAghzHUlmiVHjkaE5%2FjxEg1YwCQSCAbln";
+        String tid = "8";
         WebSocketConnectionManager manager = new WebSocketConnectionManager(
                 webSocketClient(),
                 webSocketHandler(),
-                "wss://streaming.saxotrader.com/sim/openapi/streaming/connection/connect?transport=webSockets&connectionToken=jCHxFf0mbsqo%2BiJF4wTPUDzb7cwoNneDZXa%2BjNvMmx6lzdKyOEfG7N1%2FJS9DB0sRN1GqbPn5RC3wsSK9NoORVnDa4v0bsSsxRE2EcIy8kohT%2BjHV&authorization=eyJhbGciOiJFUzI1NiIsIng1dCI6IjhGQzE5Qjc0MzFCNjNFNTVCNjc0M0QwQTc5MjMzNjZCREZGOEI4NTAifQ.eyJvYWEiOiI3Nzc3NSIsImlzcyI6Im9hIiwiYWlkIjoiMTA5IiwidWlkIjoielE3UE1EZUJzRk5COXlrMXBwYmJGdz09IiwiY2lkIjoielE3UE1EZUJzRk5COXlrMXBwYmJGdz09IiwiaXNhIjoiRmFsc2UiLCJ0aWQiOiIyMDAyIiwic2lkIjoiYTIyMjk5ZWIzZTdkNDI2N2JhYzY1YTkyMzZmOWFjMWEiLCJkZ2kiOiI4NCIsImV4cCI6IjE2MTgzMDA5MTIifQ._jGhhpeW1G3Yp8hJUQwzGzj-BE9uZtEwq0mI2TXMPvDblx6pjTXdN71Zc2xedMtXDN9mn23_HW-V3MqEZYSA2w&context=explorer_1618223191581&tid=5");
+//                "wss://streaming.saxotrader.com/sim/openapi/streaming/connection/connect?transport=webSockets&connectionToken="+connectToken+"&authorization="+bearerToken+"&context="+contextId+"&tid="+tid
+        "wss://streaming.saxotrader.com/sim/openapi/streaming/connection/connect?transport=webSockets&connectionToken=aKOob%2FmTsJTA6L7RlFbEbeVSxxQCHvLB6T%2FawtnWGzSVTRtED6sIq42B0IiEcsfXplKkapv742lzjxKbAghzHUlmiVHjkaE5%2FjxEg1YwCQSCAbln&authorization=eyJhbGciOiJFUzI1NiIsIng1dCI6IjhGQzE5Qjc0MzFCNjNFNTVCNjc0M0QwQTc5MjMzNjZCREZGOEI4NTAifQ.eyJvYWEiOiI3Nzc3NSIsImlzcyI6Im9hIiwiYWlkIjoiMTA5IiwidWlkIjoielE3UE1EZUJzRk5COXlrMXBwYmJGdz09IiwiY2lkIjoielE3UE1EZUJzRk5COXlrMXBwYmJGdz09IiwiaXNhIjoiRmFsc2UiLCJ0aWQiOiIyMDAyIiwic2lkIjoiZThjYzZlYjNiNmViNDE2ODkzNTczMThjYzU1M2FiOWIiLCJkZ2kiOiI4NCIsImV4cCI6IjE2MTg0NTA1MTgifQ.OiN5BgUA0glhCIUcYhBK7JALOD7LI3S81epdWRd_d6rn0SDcJtVZCpW1RVl81tnDTxr2ivlrz99KBeF2GHnYqA&context=explorer_1618364123021&tid=8"
+        );
 
         manager.setAutoStartup(true);
-//        HttpHeaders httpHeaders = new HttpHeaders();
-//        httpHeaders.setBearerAuth(bearerToken);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setBearerAuth(bearerToken);
 
-//        httpHeaders.setAccessControlAllowCredentials(true);
-//        httpHeaders.setAccessControlAllowOrigin("https://www.developer.saxo");
-//        httpHeaders.setOrigin("https://www.developer.saxo");
-//        httpHeaders.setUpgrade("websocket");
-//        httpHeaders.setConnection("Upgrade");
-//        manager.setHeaders(httpHeaders);
+        httpHeaders.setAccessControlAllowCredentials(true);
+        httpHeaders.setAccessControlAllowOrigin("https://www.developer.saxo");
+        httpHeaders.setBearerAuth(bearerToken);
+//        httpHeaders.set("contextId", contextId);
+        httpHeaders.setConnection("Upgrade");
+        httpHeaders.setUpgrade("websocket");
+        httpHeaders.set("Sec-WebSocket-Key", "+/HfUonDSkDCMLcFwvSdcw==");
+        httpHeaders.set("Sec-WebSocket-Version", "13");
+//        httpHeaders.set("Sec-WebSocket-Extensions", "permessage-deflate; client_max_window_bits");
+//        httpHeaders.set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36");
+        manager.setHeaders(httpHeaders);
 
         return manager;
     }
